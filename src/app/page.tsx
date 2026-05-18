ff50e10164163e5481cf4453fd06aa7daad83f87
shorkkhann <shorkkhann@gmail.com>
2026-05-18 15:51:01 +0200

    commit 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
                 {/* Ссылки / Кнопки */}
                 <div className="flex flex-wrap gap-3 pt-2">
                     <a
-                        href="https://www.linkedin.com/in/maksim-karpau/" // Замени на свою ссылку
+                        href="https://linkedin.com/in/maksim-karpau/" // Замени на свою ссылку
                         target="_blank"
                         rel="noopener noreferrer"
                         className="inline-flex items-center justify-center rounded-lg bg-neutral-900 px-4 py-2 text-sm font-medium text-neutral-50 shadow transition-colors hover:bg-neutral-800 dark:bg-neutral-50 dark:text-neutral-950 dark:hover:bg-neutral-200"
